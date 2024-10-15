b64bcafb9c31b02634cce4a224c43f125984a965
vadikghetta <helloiamvadik@mail.ru>
2024-10-15 22:32:46 +0300

    finish app

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,21 +1,12 @@
-import { homedir } from "os";
-import { join } from "path";
-import { promises } from "fs";
+import { homedir } from 'os';
+import { join } from 'path';
+import { promises } from 'fs';
 
+const filePath = join(homedir(), 'weather-data.json');
 
-const filePath = join(homedir(), "weather_data.json")
 const TOKEN_DICTIONARY = {
-    token: "token",
-    city: "city"
-}
-const isExist = async (path) =>
-{
-    try {
-        await promises.stat(path)
-        return true
-    } catch {
-        return false
-    }
+    token: 'token',
+    city: 'city'
 }
 
 const saveKeyValue = async (key, value) =>
@@ -23,19 +14,30 @@ const saveKeyValue = async (key, value) =>
     let data = {};
     if (await isExist(filePath)) {
         const file = await promises.readFile(filePath);
-        data = JSON.parse(file)
+        data = JSON.parse(file);
     }
     data[key] = value;
     await promises.writeFile(filePath, JSON.stringify(data));
-}
-const getKey = async (key) =>
+};
+
+const getKeyValue = async (key) =>
 {
     if (await isExist(filePath)) {
         const file = await promises.readFile(filePath);
-        const data = JSON.parse(file)
-        return data[key]
+        const data = JSON.parse(file);
+        return data[key];
     }
-    return undefined
-}
+    return undefined;
+};
 
-export { saveKeyValue, getKey, TOKEN_DICTIONARY }
+const isExist = async (path) =>
+{
+    try {
+        await promises.stat(path);
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+export { saveKeyValue, getKeyValue, TOKEN_DICTIONARY };
--- weather.js
+++ weather.js
@@ -1,59 +1,67 @@
 #!/usr/bin/env node
-import { getArgs } from "./helpers/args.js"
-import { getWeather } from "./services/api.service.js";
-import { printError, printHelp, printSuccess } from "./services/log.service.js";
-import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
+import { getArgs } from './helpers/args.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue } from './services/storage.service.js';
 
-
-const saveToken = async (token = "") =>
+const saveToken = async (token) =>
 {
     if (!token.length) {
-        printError("Не передан токен")
+        printError('Не передан token');
+        return;
     }
     try {
-
-        await saveKeyValue(TOKEN_DICTIONARY.token, token)
-        printSuccess("Токен сохранен")
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
+        printSuccess('Токен сохранён');
     } catch (e) {
-        printError(e.message)
+        printError(e.message);
+    }
+}
+
+const saveCity = async (city) =>
+{
+    if (!city.length) {
+        printError('Не передан город');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess('Город сохранён');
+    } catch (e) {
+        printError(e.message);
     }
 }
 
 const getForcast = async () =>
 {
     try {
-
-        const weather = await getWeather("dzhoy")
-        console.log(weather)
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+        const weather = await getWeather(city);
+        printWeather(weather, getIcon(weather.weather[0].icon));
     } catch (e) {
-        if (e?.response?.status === 404) {
-            printError("Не верно указан город")
-        } else if (e?.response?.status === 401) {
-            printError("Не верно указан токен")
+        if (e?.response?.status == 404) {
+            printError('Неверно указан город');
+        } else if (e?.response?.status == 401) {
+            printError('Неверно указан токен');
         } else {
-            printError(e.message)
-
+            printError(e.message);
         }
     }
 }
 
-
-const initCli = () =>
+const initCLI = () =>
 {
     const args = getArgs(process.argv);
-    // console.log(process.env)
     if (args.h) {
-        printHelp()
+        return printHelp();
     }
     if (args.s) {
-        //сохранить город
+        return saveCity(args.s);
     }
     if (args.t) {
-        return saveToken(args.t)        //Сохранить токен
+        return saveToken(args.t);
     }
-    getForcast()
-    // getWeather("moscow")
-    //Вывести погоду
+    return getForcast();
 };
 
-initCli()
+initCLI();
